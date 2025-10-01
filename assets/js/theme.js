92ef58b07466bfccd4b6581c205a69f89fe6234d
neveler <55753029+neveler@users.noreply.github.com>
2025-10-01 18:42:57 +0800

    支持颜色模式切换

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,39 +1,47 @@
-var themeElement = document.querySelector("[href$='main.css']");
-if (themeElement) {
-    var light = themeElement.href;
-    var dark = light.slice(0, -3) + "dark.css";
-    var mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    function handler() {
-        themeElement.href = mediaQuery.matches ? dark : light;
-    }
-    var modes = ["auto", "light", "dark"];
-    var modeNames = ["自动", "亮色", "暗色"];
-    var current = 1;
+---
+layout: null
+---
+var alternateTheme = document.createElement("link");
+var dark = "stylesheet";
+var light = "stylesheet alternate";
+alternateTheme.rel = light;
+alternateTheme.href = "{{ '/assets/css/main.dark.css' | relative_url }}"
+document.head.appendChild(alternateTheme);
+var mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+function handler() {
+    alternateTheme.rel = mediaQuery.matches ? dark : light;
+}
+window.addEventListener("DOMContentLoaded", function () {
+    var current = 0;
+    var modes = ["light", "dark", "auto"];
+    var modeNames = ["亮色", "暗色", "自动"];
+    var switchers = document.getElementsByClassName("theme-switcher");
     function themeApply(index) {
         index %= modes.length;
         if (index === current) return;
         if (modes[current] === "auto") mediaQuery.removeEventListener("change", handler);
         current = index;
+        for (var switcher of switchers) {
+            if (!switcher.href) {
+                switcher.addEventListener("click", function () {
+                    themeApply(current + 1);
+                });
+                switcher.href = "javascript:;";
+            }
+            switcher.innerText = modeNames[current];
+        }
         var mode = modes[index];
-        localStorage.setItem("theme", mode);
-        if (mode === "light") themeElement.href = light;
-        else if (mode === "dark") themeElement.href = dark;
+        localStorage.setItem("theme", index);
+        if (mode === "light") alternateTheme.rel = light;
+        else if (mode === "dark") alternateTheme.rel = dark;
         else {
             mediaQuery.addEventListener("change", handler);
             handler();
         }
     }
     themeApply(Number(localStorage.getItem("theme")) || current);
-    window.addEventListener("DOMContentLoaded", function () {
-        var switchers = document.getElementsByClassName("theme-switcher");
-        for (const switcher of switchers) {
-            switcher.addEventListener("click", function() {
-                themeApply(current + 1);
-                for (const switcher of switchers) {
-                    switcher.innerText = modeNames[current];
-                }
-            });
-            switcher.innerText = modeNames[current];
-        }
+    window.addEventListener("storage", function (event) {
+        if (event.key !== "theme") return;
+        themeApply(Number(event.newValue) || 0);
     });
-}
+});
