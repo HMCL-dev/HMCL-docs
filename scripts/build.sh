f3dc88f5785127feebada2e06032a77a8411166f
neveler <55753029+neveler@users.noreply.github.com>
2025-10-04 19:36:32 +0800

    支持多语言

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,15 +15,26 @@ for config in ./_config.*.yml; do
     [ -f "$config" ] || continue
     language="${config#./_config.}"
     language="${language%.yml}"
-    for data in ./src/data/*."$language".*; do
-        [ -f "$data" ] || continue
-        dest="./src/data/${data#./src/data/}"
-        dest="${dest/.$language./.}"
-        mv "$data" "$dest"
-    done
+    if [ "$language" != "default" ]; then
+        for data in ./src/data/*."$language".*; do
+            [ -f "$data" ] || continue
+            dest="${data/.$language./.}"
+            mv "$data" "$dest"
+        done
+        find src -type f -name "*.md" ! -name "*.$language.md" | while read -r file; do
+            target="${file%.md}.en.md"
+            if [ ! -f "$target" ]; then
+                cp "$file" "$target"
+            fi
+        done
+    fi
     bundle exec jekyll build --trace --verbose --destination "_site_$language" --config "_config.yml,_config.$language.yml"
     cd "_site_$language"
-    find . -type f -name "*.$language.html" -exec cp --parents {} ../_site \;
+    if [ "$language" != "default" ]; then
+        find . -type f -name "*.$language.html" -exec cp --parents {} ../_site \;
+    else
+        find . -type f -name "*.html" -exec cp --parents {} ../_site \;
+    fi
     cd ..
     rm -rf "_site_$language"
     cp -r ./data ./src/data
